5d6196e6bad9a99d66b6f667f853e8d1b669d232
BOISE\Stephanie_Labastida <slabastida@cityofboise.org>
2018-10-18 17:10:56 -0600

    Initial commit

---
+++ clock_func.js
@@ -0,0 +1,30 @@
+function updateClock() {
+  var currentDateTime = new Date();
+  var timeOfDay;
+
+  var currentHours = currentDateTime.getHours();
+  if (currentHours < 12) {
+    timeOfDay = "AM";
+  } else {
+    currentHours -= 12;
+    timeOfDay = "PM";
+  }
+
+  var currentMins = currentDateTime.getMinutes();
+  if (currentMins < 10) {
+    currentMins = "0" + currentMins;
+  }
+  var currentSecs = currentDateTime.getSeconds();
+  if (currentSecs < 10) {
+    currentSecs = "0" + currentSecs;
+  }
+
+  var currentTimeString = currentHours + ":" + currentMins + ":" + currentSecs + " " + timeOfDay;
+
+  document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+}
+
+document.getElementById("clock").onload = setInterval(updateClock, 1000);
+
+updateClock();
+debug();
